4531d32940d294b02f4e553b8a508362b9e3fcfe
edu-mryo <edu.mryo@gmail.com>
2023-02-15 00:04:23 +0800

    Week 3 Discussion Assignment

---
+++ programming_1/week3/discussion.java
@@ -0,0 +1,17 @@
+/**
+ * discussion
+ */
+public class discussion {
+
+    public static void subtract(int a, int b) {
+        System.out.println(a-b);
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        subtract(a, b);
+
+    }
+}
